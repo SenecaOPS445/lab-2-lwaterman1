e78f61f94f8f1464f0d3017a2551f540772cd6f7
Liam Waterman <lwaterman1@myseneca.ca>
2025-01-24 12:23:02 -0500

    lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author: Liam Waterman
+#Author ID: 123151235
+#Date Created: 2025/01/24 
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print (timer)
+    timer = timer - 1
+print ("blast off!")
