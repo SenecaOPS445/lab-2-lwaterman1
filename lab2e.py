d0eb3e75a66e912a337d482c8e60cf4bf8d79235
Liam Waterman <lwaterman1@myseneca.ca>
2025-01-24 12:22:47 -0500

    lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#Author: Liam Waterman
+#Author ID: 123151235
+#Date Created: 2025/01/24 
+
+timer = 10
+while timer != 0:
+    print (timer)
+    timer = timer - 1
+print ("blast off!")
